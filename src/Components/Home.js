e544320977e9e7a429718ca2e1bc6d59de2345d4
Joshua Israel <joshisrael93@gmail.com>
2020-08-30 22:06:08 -0400

    token attempt one

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,22 +2,32 @@ import React, { useState, useEffect} from 'react';
 
 
 
+
 function Home () {
     const spotKey = process.env.SPOT_KEY;
-    let [artist, setArtist] = useState('');
-    let [season, setSeason] = useState('neutral');
+    const spotId = process.env.SPOT_ID;
+    const auth = btoa(`${spotId}:${spotKey}`)
+	let [artist, setArtist] = useState('');
+	let [season, setSeason] = useState('neutral');
 
-    useEffect(() => {
-        fetch('http')
+	useEffect(() => {
+	      fetch({
+        url:'https://accounts.spotify.com/api/token',
+        method: 'POST',
+        headers: {Authorization: `Basic ${auth}`},
+        body: {"grant_type": "client_credentials"},
+        json: true
     })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+})
 
-        return (
-            <div>
-                
-            </div>
-        );
-
-    }
+	return (
+		<div>
+			<h1>howdy earth</h1>
+		</div>
+	);
+}
 
 
 export default Home;
